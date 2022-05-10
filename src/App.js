f052622895dcafdc4d8bad0c0572401447967c0f
黄立铭 <frank@zasoft.cn>
2022-05-10 12:05:07 +0800

    loginpage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
 import './App.css';
+import Lobby from 'Components/Lobby';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const App =() => {
+  const[connection, setConnection] = useState();
+  const[messages, setMessages] = useState([]);
+  const[users,setUsers] =useState([]);
+  const joinRoom = async (user,room) =>{
+    try {
+      const _cnn = new HubConnectionBuilder()
+                  .withUrl("http://localhost:5000/chat")
+                  .configureLogging(LogLevel.Information)
+                  .build();
+          _cnn.on("ReceiveMessage",(user,message) =>{
+            setMessages(messages =>[...messages,{user,message}]);
+          });
+          _cnn.on("UsersInRoom",(users)=>{
+            setUsers(users);
+          });
+          _cnn.onclose(e=>{
+            setConnection();
+            setUsers([]);
+            setMessages([]);
+          });
+         await _cnn.start();
+         await _cnn.invoke("JoinRoom",{user,room});
+         setConnection(_cnn);
+        
+    } catch (error) {
+      console.log(error);
+    }
+     
+
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <h2>MyChat</h2>
+       <hr className='line' />
+       <Lobby joinRoom={joinRoom} />
     </div>
   );
 }
